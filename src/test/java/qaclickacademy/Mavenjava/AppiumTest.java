a070299216c15dfc62f6b8981b7f42a76b0cd1c9
HarishS1987 <harish.sukant88@gmail.com>
2022-01-14 12:19:27 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAndroidApp()
+	{
+		System.out.println("NativeAndroidApp");
+	}
+	
+	@Test
+	public void IOS_APP() 
+	{
+		System.out.println("IOS_APP");	
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Mavenjava;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {	
+		
+		@Test
+		public void BrowserAutomation()
+		{
+			System.out.println("Web Automation");
+		}
+		
+		@Test
+		public void ElementsUI()
+		{
+			System.out.println("Web UI Automation");
+			System.out.println("any");
+			System.out.println("any will be fine");
+			System.out.println("any");
+		}
+		
+}
